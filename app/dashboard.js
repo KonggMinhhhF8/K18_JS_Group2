eb082e63b16fdc3c1df2bcf962914a3f4aa3820e
anhson1207 <anhsondoraemon@gmail.com>
2026-03-28 19:11:51 +0700

    feat(login)

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,3 +1,26 @@
 import { renderSidebar } from "./js/components/sidebar.js"
 const container=document.querySelector(".container");
-container.insertAdjacentHTML("afterbegin",renderSidebar("dashboard"));
+const userTrigger = document.getElementById("userTrigger");
+const logoutBtn = document.getElementById("logoutBtn");
+container.insertAdjacentHTML("afterbegin", renderSidebar("dashboard"));
+
+if (userTrigger && logoutBtn) {
+    userTrigger.addEventListener("click", (e) => {
+        e.stopPropagation();
+        logoutBtn.classList.toggle("show");
+    });
+
+    document.addEventListener("click", (e) => {
+        const isClickInsideHeaderActions = e.target.closest(".header-actions");
+
+        if (!isClickInsideHeaderActions) {
+            logoutBtn.classList.remove("show");
+        }
+    });
+}
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+        window.location.href = "../app/login.html";
+    });
+}
